0d3dc14ee87e673f1d3d3dd5d7a3e77b6c64c265
Test <zakharseva1980@gmail.com>
2018-12-01 14:11:56 +0300

    add vagrantfile + storeInit

--- Store/Models/StoreContext.cs
+++ Store/Models/StoreContext.cs
@@ -8,7 +8,13 @@ namespace Store.Models
     {
         public StoreContext()
             : base("name=StoreContext")
-        {}
+        {
+            Database.SetInitializer<StoreContext>(new CreateDatabaseIfNotExists<StoreContext>());
+
+            //Database.SetInitializer<SchoolDBContext>(new DropCreateDatabaseIfModelChanges<SchoolDBContext>());
+            //Database.SetInitializer<SchoolDBContext>(new DropCreateDatabaseAlways<SchoolDBContext>());
+            //Database.SetInitializer<SchoolDBContext>(new SchoolDBInitializer());
+        }
         public DbSet<Product> Products { get; set; }
     }
 }
