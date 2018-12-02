461c541f22a2ed256fc21142b8d6034924edb71e
Test <zakharseva1980@gmail.com>
2018-12-03 02:01:52 +0300

    add configsql

---
+++ Vagrant/configDB.ps1
@@ -0,0 +1,6 @@
+﻿sqlcmd -S .\SQLEXPRESS -Q "CREATE database StoreDB"
+sqlcmd -S .\SQLEXPRESS -i C:\inetpub\wwwroot\5\Vagrant\configStoreDB.sql
+
+#sqlcmd -S .\SQLEXPRESS -i C:\configStoreDB.sql
+#sqlcmd -S .\SQLEXPRESS -i C:\StoreDB_data_scheme.sql
+#sqlcmd -S .\SQLEXPRESS -i C:\IIS.sql
