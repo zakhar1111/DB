a5e7ecf735170d4a23e7b8171bbc6ed39fd63c0e
zakhar1111 <40389817+zakhar1111@users.noreply.github.com>
2018-12-05 15:32:35 +0300

    Update getRepository.ps1

--- Vagrant/getRepository.ps1
+++ Vagrant/getRepository.ps1
@@ -1,7 +1,4 @@
 ﻿$GitUrl = "https://github.com/zakhar1111/DB.git"
 $Destination = "C:\\inetpub\\wwwroot\\5"
 git clone $GitUrl $Destination
-#Start-Sleep -Seconds 30
-#Invoke-Command $Destination\Store\build_Store.cmd
-#cmd /c $Destination\Store\build_Store.cmd
 msbuild "$Destination\\Store\\store.csproj" /t:build
