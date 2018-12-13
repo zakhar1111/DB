c028959de851c6b24aac49106503e53e842e9144
zakhar1111 <40389817+zakhar1111@users.noreply.github.com>
2018-12-14 02:36:53 +0300

    Update DiscontTest.cs

--- Store.Tests/DiscontTest.cs
+++ Store.Tests/DiscontTest.cs
@@ -13,7 +13,7 @@ namespace Store.Tests
                   new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
                   new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
                 };
-        [TestMethod]
+        //[TestMethod]
         public void TestMethod1()
         {
            
